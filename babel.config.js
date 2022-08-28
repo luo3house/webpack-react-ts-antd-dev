b0c7b7ced75647f6d83d6108b03ca85ed4470e1e
luoyz <luoyz2349@outlook.com>
2022-08-28 17:23:41 +0800

    feat: babel plugin for import react automatically

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,12 @@ module.exports = {
     "@babel/preset-env",
     "@babel/preset-typescript",
     "@babel/preset-react"
+  ],
+  plugins: [
+    ["babel-plugin-auto-import", {
+      declarations: [
+        { default: "React", "path": "react" }
+      ]
+    }]
   ]
 }
